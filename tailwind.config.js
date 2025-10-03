e5d1db830577c21cf92ad7839530055f3b5e2339
kpx-136 <kpx.136@gmail.com>
2025-10-03 15:49:54 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./*.{html,js}" // Tells the extension to scan HTML and JS files for classes
+    ], 
+    theme: {
+        extend: {
+            
+        },
+    },
+    plugins: [],
+}
